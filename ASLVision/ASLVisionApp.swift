971cea94c3e7a4b007b112b19ffc40a0d0b8dc42
Théo Arrouye <theo@arrouye.net>
2021-02-20 13:52:38 -0800

    Initial Commit

---
+++ ASLVision/ASLVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASLVisionApp.swift
+//  ASLVision
+//
+//  Created by Théo Arrouye on 2/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ASLVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
